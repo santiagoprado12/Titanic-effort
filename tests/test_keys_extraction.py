f3ce923482b5c6eef3b05bbc22cf16a37a7b6cbc
Santiago Prado <74788002+santiagoprado12@users.noreply.github.com>
2023-08-12 21:54:45 -0500

    create tests for keys_extraction class

---
+++ tests/test_keys_extraction.py
@@ -0,0 +1,51 @@
+
+from src.utils.keys_extraction import *
+from unittest import TestCase
+from cryptography.fernet import Fernet
+import os
+
+
+def test_get_secret_key_input(monkeypatch):
+
+    monkeypatch.setattr('builtins.input', lambda _: "input_string")
+    key_extract = KeysExtraction()
+
+    assert key_extract.secret_key == "input_string"
+
+
+def test_get_secret_key_env_var():
+
+    os.environ["SECRET_KEY"] = "env_variable"
+    key_extract = KeysExtraction()
+
+    assert key_extract.secret_key == "env_variable"
+
+
+def test_get_decrypt_file(tmpdir):
+    
+    def encrypt_and_save_file(fpath, keys):
+    
+        secret_key = Fernet.generate_key()  
+        cipher_suite = Fernet(secret_key)
+        encrypted_data = cipher_suite.encrypt(str.encode(json.dumps(keys)))
+        with open(fpath, "wb") as file:
+            file.write(encrypted_data)
+        return secret_key
+    
+    keys = {
+        "access_key": "test_access_key",
+        "secret_key": "test_secret_key",
+    }
+
+    fpath = f"{tmpdir}/encrypted_data_test.txt"
+
+    secret_key = encrypt_and_save_file(fpath, keys)
+
+    os.environ["SECRET_KEY"] = secret_key.decode()
+
+    key_extract = KeysExtraction(path=fpath)
+    decrypted_keys = key_extract.decrypt_file()
+
+    TestCase().assertDictEqual(decrypted_keys, keys)
+
+
