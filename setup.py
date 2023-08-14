29711838e1a030c63d9d9ea313701be55cd9d7bb
Santiago Prado <74788002+santiagoprado12@users.noreply.github.com>
2023-08-14 10:54:48 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
-from setuptools import setup
-if __name__ == '__main__':
-    setup()
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r") as f:
+    install_requires = f.readlines()
+
+setup(
+    name="your-package-name",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=install_requires
+)
