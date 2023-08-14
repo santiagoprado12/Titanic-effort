989788cc09f9e0f07fe6dd20f23d381860e9aefa
Santiago Prado <74788002+santiagoprado12@users.noreply.github.com>
2023-08-13 22:58:19 -0500

    change functions names

--- src/utils/keys_extraction.py
+++ src/utils/keys_extraction.py
@@ -20,16 +20,16 @@ class KeysExtraction:
         VAR_NAME = "SECRET_KEY"     
         #see if env variable exists
         if  VAR_NAME in os.environ:
-            self.secret_key = os.environ[VAR_NAME]
+            self._secret_key = os.environ[VAR_NAME]
         else:
-            self.secret_key = input("Enter the secret key: ")
+            self._secret_key = input("Enter the secret key: ")
 
     def decrypt_file(self) -> dict:
 
         with open(self.path, "rb") as file:
             encrypted_data = file.read()
 
-        cipher_suite = Fernet(str.encode(self.secret_key))
+        cipher_suite = Fernet(str.encode(self._secret_key))
         decrypted_data = cipher_suite.decrypt(encrypted_data)
         self.decrypted_json_data = json.loads(decrypted_data.decode())
 
