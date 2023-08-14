9f98e02217c6f323dde312d81d077111e94a1719
Santiago Prado <74788002+santiagoprado12@users.noreply.github.com>
2023-08-14 10:57:12 -0500

    fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("requirements.txt", "r") as f:
     install_requires = f.readlines()
 
 setup(
-    name="your-package-name",
+    name="titanic-effort",
     version="0.1.0",
     packages=find_packages(),
     install_requires=install_requires
