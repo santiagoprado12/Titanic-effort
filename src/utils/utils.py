ce15d42c296d550ebbc7df84893309d15960043b
Santiago Prado <74788002+santiagoprado12@users.noreply.github.com>
2023-08-13 22:57:55 -0500

    create utils file

---
+++ src/utils/utils.py
@@ -0,0 +1,10 @@
+import subprocess
+
+def run_makefile(target):
+
+    try:
+        subprocess.run(['make', target], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running Makefile target '{target}':")
+        print(e)
+        raise e
