c4d8589c22c3346520e4ec5445dae5d7d033dd38
Santiago Prado <74788002+santiagoprado12@users.noreply.github.com>
2023-08-14 21:14:34 -0500

    add validation and testing

--- src/CLI.py
+++ src/CLI.py
@@ -5,6 +5,7 @@ import typer
 import src.ml_core.train as train_model
 from src.utils.keys_extraction import KeysExtraction
 from src.utils.utils import run_makefile
+import src.ml_core.validation as validation_model
 
 app = typer.Typer() # Create a new typer.Typer() application.
 
@@ -20,7 +21,6 @@ def train(model: Annotated[Optional[List[ModelType]], typer.Option(..., "-m", "-
           threshold: float = typer.Option(None, "--acc-threshold", "-th", help="accuracy threshold for the model to be registered (between 0 and 1))")):
     """Train the model"""
 
-    
     if threshold is None:
         KeysExtraction().set_env_variables()
         run_makefile("dvc-pull-data")
@@ -31,23 +31,48 @@ def train(model: Annotated[Optional[List[ModelType]], typer.Option(..., "-m", "-
         train_model.main([mod.value for mod in model], threshold)
     else:
         typer.echo("Invalid input. Please enter a float number between 0 and 1.")
+        raise typer.Abort()
 
     if register:
         run_makefile("register_model")
 
-@app.command()
-def main(user: Annotated[Optional[List[str]], typer.Option()] = None):
-    if not user:
-        print("No provided users")
-        raise typer.Abort()
-    for u in user:
-        print(f"Processing user: {u}")
 
 @app.command()
-def say_bye(name: str):
-    """Say bye to users"""
+def validation():
+    """Validate the model"""
 
-    print(f'Good bye {name}')
+    KeysExtraction().set_env_variables()
+    run_makefile("dvc-pull-data")
+    run_makefile("dvc-pull-model")
+    validation_model.main()
+
+
+@app.command()
+def dataup(only_train: bool = typer.Option(False, "--only-train", "-t", help="only register the train data"),
+            only_validation: bool = typer.Option(False, "--only-validation", "-v", help="only register the validation data")):
+    """Update the data in the registry"""
+
+    if only_train:
+        KeysExtraction().set_env_variables()
+        run_makefile("dvc-push-train-data")
+    
+    elif only_validation:
+        KeysExtraction().set_env_variables()
+        run_makefile("dvc-push-validation-data")
+    
+    else:
+        KeysExtraction().set_env_variables()
+        run_makefile("dvc-push-data")
+
+
+@app.command()
+def test(coverage: bool = typer.Option(False, "--coverage", "-c", help="run the tests with coverage", show_default=False)):
+    """Run the tests"""
+
+    if coverage:
+        run_makefile("test-coverage")
+    else:
+        run_makefile("test")
 
 
 if __name__ == '__main__':
