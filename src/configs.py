880fd960a5165d8b2c8ee69639be59aa53a72153
Santiago Prado <74788002+santiagoprado12@users.noreply.github.com>
2023-08-15 12:18:04 -0500

    create configs

--- src/configs.py
+++ src/configs.py
@@ -5,10 +5,38 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
 from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
 
 
 class Configs:
+    """Class to store all the configurations of the project
+
+    Attributes:
+        search_tecnique (dict): Dictionary with the search technique to use
+        models (list): List of dictionaries with the models to use
+    """
+
+    search_tecnique = {
+        'technique': RandomizedSearchCV,
+        'params': {
+            'n_iter': 15,
+            'cv': 5,
+            'verbose': 0,
+            'n_jobs': -1
+        }         
+    }
+
+    # search_tecnique = {
+    #     'technique': GridSearchCV,
+    #     'params': {
+    #         'cv': 5,
+    #         'verbose': 0,
+    #         'n_jobs': -1
+    #     }
+    # }
+
 
     models = [
         {
@@ -23,8 +51,8 @@ class Configs:
                 'bootstrap': [True, False],
                 'random_state': [42]
             }
-        },
-        {
+        }
+        ,{
             'name': 'gradient_boosting',
             'model': GradientBoostingClassifier(),
             'params': {
@@ -36,15 +64,24 @@ class Configs:
                 'subsample': [0.8, 0.9, 1.0],
                 'random_state': [42]
             }
-        },
-        {
+        }
+        ,{
             'name': 'knn',
             'model': KNeighborsClassifier(),
             'params': {
-            'n_neighbors': np.arange(1, 21),
-            'weights': ['uniform', 'distance'],
-            'metric': ['euclidean', 'manhattan']
+                'n_neighbors': np.arange(1, 21),
+                'weights': ['uniform', 'distance'],
+                'metric': ['euclidean', 'manhattan']
+            }
         }
-        }
+        # ,{
+        #     'name': 'svm',
+        #     'model': SVC(),
+        #     'params': {
+        #         'C': [0.1, 1, 10, 100],
+        #         'gamma': [1, 0.1, 0.01, 0.001],
+        #         'kernel': ['rbf', 'poly', 'sigmoid']
+        #     }
+        # }
 
     ]
