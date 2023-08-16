cc61f7ec15e2940862ae4d95a5987ea27078b0af
Santiago Prado <74788002+santiagoprado12@users.noreply.github.com>
2023-08-15 23:13:44 -0500

    modify data functions

--- src/utils/data_functions.py
+++ src/utils/data_functions.py
@@ -80,6 +80,17 @@ def generate_validation_report(model, X_test, y_test, output_file='validation_re
 
 
 
+def preprocess_features(data: pd.DataFrame) -> pd.DataFrame:
+
+    titanic_data = data.copy()
+    titanic_data.drop(['Cabin', 'PassengerId', 'Name',
+                      'Ticket'], axis=1, inplace=True)
+    titanic_data['FamilySize'] = titanic_data['SibSp'] + titanic_data['Parch']
+    titanic_data['IsAlone'] = 0
+    titanic_data.loc[titanic_data['FamilySize'] == 0, 'IsAlone'] = 1
+
+    return titanic_data
+
 
 def preprocess_data(data: pd.DataFrame, target_column: str) -> pd.DataFrame:
     """Preprocess the data
@@ -92,12 +103,8 @@ def preprocess_data(data: pd.DataFrame, target_column: str) -> pd.DataFrame:
         - Parch
     """
 
-    titanic_data = data.copy()
-    titanic_data.drop(['Cabin', 'PassengerId', 'Name',
-                      'Ticket'], axis=1, inplace=True)
-    titanic_data['FamilySize'] = titanic_data['SibSp'] + titanic_data['Parch']
-    titanic_data['IsAlone'] = 0
-    titanic_data.loc[titanic_data['FamilySize'] == 0, 'IsAlone'] = 1
+    titanic_data = preprocess_features(data)
+    
 
     X = titanic_data.drop(target_column, axis=1)
     y = titanic_data[target_column]
